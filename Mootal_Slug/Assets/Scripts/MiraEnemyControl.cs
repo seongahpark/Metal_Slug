c11f23a79ec7f8111c139cf64771f7983e23e919
seongahpark <sksmsekekek21@naver.com>
2021-10-25 11:57:07 +0900

    폭탄 데미지, 피격효과 추가, 플레이어와 적 bullet 상호작용

--- Mootal_Slug/Assets/Scripts/MiraEnemyControl.cs
+++ Mootal_Slug/Assets/Scripts/MiraEnemyControl.cs
@@ -48,6 +48,14 @@ public class MiraEnemyControl : MonoBehaviour
         }
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "P_Bomb")
+        {
+            StartCoroutine(ec.Blink(rend));
+            miniEnemyHp -= 3;
+        }
+    }
     private void changeState()
     {
         state = Random.Range(0, 3);
